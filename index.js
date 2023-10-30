598a95f345ee7c2808e9c2f9d7b21c3bc5464b33
Federico Coronel <fedec34@gmail.com>
2023-10-30 13:33:26 -0300

    add morgan logs to proyect

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
 const port = process.env.PORT || 3000; // Choose a suitable port
-const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   const htmlResponse = `
