6505ada70686ba7fd14484b44880ffff341a1a21
fcoronel89 <fedec34@gmail.com>
2023-11-03 15:29:48 -0300

    refactor whatsapp controller

--- src/routes/whatsapp/whatsapp.controller.js
+++ src/routes/whatsapp/whatsapp.controller.js
@@ -1,25 +1,67 @@
 const axios = require("axios");
 require("dotenv").config();
 
+const fromPhoneNumberId = process.env.WHATSAPP_FROM_NUMBER_ID;
+const accessToken = process.env.WHATSAPP_ACCESS_TOKEN;
+
+function getDataToSend(shift, professional, confirmationType) {
+  const templateName =
+    confirmationType === "client"
+      ? "shift_confirmation_client"
+      : "shift_confirmation_professional ";
+  const urlToConfirm =
+    confirmationType === "client"
+      ? "confirmar-turno-cliente/"
+      : "confirmar-turno-profesional/";
+  const secondParameter =
+    confirmationType === "client"
+      ? `${professional.firstName} ${professional.lastName}`
+      : shift.service;
+  return {
+    messaging_product: "whatsapp",
+    recipient_type: "individual",
+    to: "542234215869",
+    type: "template",
+    template: {
+      name: templateName,
+      language: {
+        code: "es_AR",
+      },
+      components: [
+        {
+          type: "body",
+          parameters: [
+            {
+              type: "text",
+              text: `${shift.shiftDate} a las ${shift.time}`,
+            },
+            {
+              type: "text",
+              text: `${secondParameter}`,
+            },
+          ],
+        },
+        {
+          type: "button",
+          sub_type: "url",
+          index: "0",
+          parameters: [
+            {
+              type: "text",
+              text: `${urlToConfirm}${shift.id}`,
+            },
+          ],
+        },
+      ],
+    },
+  };
+}
+
 async function sendWhatsappMessage(req, res) {
   try {
     const { shift, professional, confirmationType } = req.body;
-
-    const fromPhoneNumberId = process.env.WHATSAPP_FROM_NUMBER_ID;
-    const accessToken = process.env.WHATSAPP_ACCESS_TOKEN;
     //const recipientPhoneNumber = req.body.recipientPhoneNumber;
-    const templateName =
-      confirmationType === "client"
-        ? "shift_confirmation_client"
-        : "shift_confirmation_professional ";
-    const urlToConfirm =
-      confirmationType === "client"
-        ? "confirmar-turno-cliente/"
-        : "confirmar-turno-profesional/";
-    const secondParameter =
-      confirmationType === "client"
-        ? `${professional.firstName} ${professional.lastName}`
-        : shift.service;
+
     console.log("body", req.body);
 
     const url = `https://graph.facebook.com/v18.0/${fromPhoneNumberId}/messages`;
@@ -28,44 +70,7 @@ async function sendWhatsappMessage(req, res) {
       "Content-Type": "application/json",
     };
 
-    const data = {
-      messaging_product: "whatsapp",
-      recipient_type: "individual",
-      to: "542234215869",
-      type: "template",
-      template: {
-        name: templateName,
-        language: {
-          code: "es_AR",
-        },
-        components: [
-          {
-            type: "body",
-            parameters: [
-              {
-                type: "text",
-                text: `${shift.shiftDate} a las ${shift.time}`,
-              },
-              {
-                type: "text",
-                text: `${secondParameter}`,
-              },
-            ],
-          },
-          {
-            type: "button",
-            sub_type: "url",
-            index: "0",
-            parameters: [
-              {
-                type: "text",
-                text: `${urlToConfirm}${shift.id}`,
-              },
-            ],
-          },
-        ],
-      },
-    };
+    const data = getDataToSend(shift, professional, confirmationType);
 
     // Send the WhatsApp message
     const response = await axios.post(url, data, { headers });
@@ -74,7 +79,7 @@ async function sendWhatsappMessage(req, res) {
     res.json({ ok: true });
   } catch (error) {
     console.error("Error sending message:", error);
-    res.status(500).json({ ok:false, error: "Failed to send message" });
+    res.status(500).json({ ok: false, error: "Failed to send message" });
   }
 }
 
