4bf497c9c4d10369f996a2d48de9099b0df1d6a3
fcoronel89 <fedec34@gmail.com>
2023-11-15 10:07:14 -0300

    change server settings https for production

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const https = require("https");
+const http = require('http');
 const app = require("./app");
 
 require("dotenv").config();
@@ -8,13 +9,19 @@ const { mongoConnect } = require("./services/mongo");
 
 const port = process.env.PORT || 3000; // Choose a suitable port
 
-const server = https.createServer(
-  {
-    key: fs.readFileSync("src/key.pem"),
-    cert: fs.readFileSync("src/cert.pem"),
-  },
-  app
-);
+if (process.env.NODE_ENV === 'production') {
+  // Running in production, use regular HTTP
+  server = http.createServer(app);
+} else {
+  // Running in development, use HTTPS with your own certificates
+  server = https.createServer(
+    {
+      key: fs.readFileSync("src/key.pem"),
+      cert: fs.readFileSync("src/cert.pem"),
+    },
+    app
+  );
+}
 
 async function startsServer() {
   await mongoConnect();
