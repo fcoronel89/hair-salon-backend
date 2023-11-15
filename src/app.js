fde98b234f76ab768e66675f95f2a27fd3434c1d
fcoronel89 <fedec34@gmail.com>
2023-11-15 10:56:08 -0300

    add store session in mongo db

--- src/app.js
+++ src/app.js
@@ -4,16 +4,18 @@ const morgan = require("morgan");
 const passport = require("passport");
 const helmet = require("helmet");
 const expressSession = require("express-session");
+const MongoStore = require("connect-mongo");
 
 require("./config/passport-config")(passport);
 
 const api = require("./routes/api");
+
 require("dotenv").config();
 
 const app = express();
 
 const corsOptions = {
-  origin: 'https://localhost:5173', // Replace with the actual origin of your React app
+  origin: process.env.FRONTEND_URL, // Replace with the actual origin of your React app
   credentials: true, // Allow credentials (cookies, sessions)
 };
 
@@ -23,12 +25,14 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(
   expressSession({
+    //store: sessionStore, // Use the imported session store
     secret: process.env.COOKIE_KEY_1, // Replace with a strong secret key
     resave: false,
     saveUninitialized: false,
     cookie: {
       maxAge: 60 * 24 * 60 * 60 * 1000, // Session duration in milliseconds
     },
+    store: MongoStore.create({mongoUrl: process.env.MONGO_URL, collectionName: 'sessions'})
   })
 );
 app.use(passport.initialize());
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const https = require("https");
-const http = require('http');
+const http = require("http");
 const app = require("./app");
 
 require("dotenv").config();
@@ -8,26 +8,38 @@ require("dotenv").config();
 const { mongoConnect } = require("./services/mongo");
 
 const port = process.env.PORT || 3000; // Choose a suitable port
+let server;
 
-if (process.env.NODE_ENV === 'production') {
-  // Running in production, use regular HTTP
-  server = http.createServer(app);
-} else {
-  // Running in development, use HTTPS with your own certificates
-  server = https.createServer(
-    {
-      key: fs.readFileSync("src/key.pem"),
-      cert: fs.readFileSync("src/cert.pem"),
-    },
-    app
-  );
+async function startServer() {
+  try {
+    // Establish MongoDB connection
+    await mongoConnect();
+
+    // Create HTTP or HTTPS server based on the environment
+    if (process.env.NODE_ENV === "production") {
+      // Running in production, use regular HTTP
+      server = http.createServer(app);
+    } else {
+      // Running in development, use HTTPS with your own certificates
+      server = https.createServer(
+        {
+          key: fs.readFileSync("src/key.pem"),
+          cert: fs.readFileSync("src/cert.pem"),
+        },
+        app
+      );
+    }
+
+    // Start the server
+    server.listen(port, () => {
+      console.log(`Server is running in http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the application on connection error
+  }
 }
 
-async function startsServer() {
-  await mongoConnect();
-  server.listen(port, () => {
-    console.log(`Server is runing in http://localhost:${port}`);
-  });
-}
+startServer();
 
-startsServer();
+module.exports = server;
