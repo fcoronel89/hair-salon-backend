57ae362032d34dbe049fd8cc52645ae5f82bf0e7
fcoronel89 <fedec34@gmail.com>
2024-02-23 15:52:32 -0300

    disabled crone

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ app.use(passport.session());
 app.use("/v1", api);
 
 // Scheduling the backup everyday at 00:00:00 (using node-cron) 0 0 * * *
-cron.schedule("0 0 * * *", () => backupMongoDB());
+//cron.schedule("0 0 * * *", () => backupMongoDB());
 //restoreMongoDB();
 
 module.exports = app;
